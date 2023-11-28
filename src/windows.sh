145fb3f111f44b1dfe0fc1743fe424da8f858763
Luis Blanco <luisblanco1337@gmail.com>
2023-11-28 23:21:47 +0400

    Fix windows mt lib

--- src/windows.sh
+++ src/windows.sh
@@ -2,6 +2,8 @@
 	cd src/libuiohook/build
 	
 	cmake -A x64 \
+		-DCMAKE_POLICY_DEFAULT_CMP0091=NEW  \
+		-DCMAKE_MSVC_RUNTIME_LIBRARY="MultiThreaded" \
 		-DBUILD_SHARED_LIBS=OFF \
 		-DBUILD_DEMO=OFF \
 		-DCMAKE_INSTALL_PREFIX=../installed \
