da4549a17ad4130d859c4838700a7ca42607749a
Luis Blanco <luisblanco1337@gmail.com>
2023-11-28 21:27:40 +0400

    Add linux libs

--- src/update-linux.sh
+++ src/update-linux.sh
@@ -1,2 +1,3 @@
 sudo apt-get update -qq
-sudo apt-get install -qq xorg-dev
+sudo apt-get install -qq libx11-dev libxtst-dev libxt-dev libxinerama-dev libx11-xcb-dev libxkbcommon-dev libxkbcommon-x11-dev libxkbfile-dev
+
